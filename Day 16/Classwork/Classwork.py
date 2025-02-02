2e739a03d96baa6e1301b87c086ebb8850f7b29a
dachipapashvili <dachipapashvili4@gmail.com>
2025-02-02 14:28:07 +0400

    commit

---
+++ Day 16/Classwork/Classwork.py
@@ -0,0 +1,23 @@
+Num_1=input("enter number")
+print(int(Num_1) * 5)
+
+#2 საკლასო
+
+a=True
+b=False
+
+not_a=a and a or False
+not_b=b and b or False
+
+
+print(not_a,not_b)
+
+
+
+
+#3 საკლასო
+
+ar=20
+ar=70
+print(ar)
+
---
+++ Day 16/Homework/Homework.py
@@ -0,0 +1,52 @@
+
+Num1=10
+
+Num2=30
+
+which=(str(("Num1 is True")))
+
+print(which)
+
+#2 დავალება
+
+Num1=20
+Num2=30
+
+answer=(Num1 < Num2)
+
+print(answer)
+
+
+#3 დავალება
+
+
+Num1=40
+Num2=39
+
+answer=(Num1 > Num2)
+print(answer)
+
+
+#4 დავალება
+age=input("enter your age")
+
+if 6 <= int(age) <= 12 :
+    print(True)
+else:
+    print(False)
+
+
+#5 დავალება
+
+a=True
+b=False
+
+not_a=a and a or False
+not_b=b and b or False
+
+
+print(not_a,not_b)
+
+
+
+
