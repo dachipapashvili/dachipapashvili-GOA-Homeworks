804431fa9e22314b4e7240fd6c83ad4375b62423
dachipapashvili <dachipapashvili4@gmail.com>
2025-03-22 23:09:20 +0400

    commit

---
+++ Day 22/Classwork/Classwork.py
@@ -0,0 +1,97 @@
+for i in range (0, 101, 2):
+    print(i)
+
+
+#2 saklaso
+
+
+for i in range (1, 101 , 1):
+    print(i)
+
+
+#3 saklaso
+
+apple= 0
+for i in range(1, 101):
+    apple = apple + i
+print(apple)
+
+
+#4 saklaso
+
+
+
+milk = 0
+for i in range(1, 101 , 2):
+    milk = milk + i
+print(milk)
+
+
+
+#5 saklaso
+
+
+
+bread = 0
+for i in range(1, 101 , 1):
+   bread = bread + i
+print(bread)
+
+
+
+#6 saklaso
+seats = 5
+while seats > 0:
+    print(seats)
+    seats = seats - 1
+
+
+#7 saklaso
+
+count = 3
+while count < 6:
+    print(count)
+    count = count + 1
+
+
+# 8 saklaso
+name= 0
+userinput=input("რიცხვი")
+while int(userinput) > name:
+    print(name)
+    name+=1
+
+
+
+#9 saklaso
+password= 0
+userinput=input("პაროლის შემოწმება")
+if int(userinput) == password:
+    print("სწორია")
+    name+=1
+
+
+
+
+if int(userinput ) != password:
+    print("არასწორია")
+    name+=1
+
+#10 saklaso
+number=7
+numberinput=int(input('>'))
+while numberinput != number:
+    print("არასწორია")
+    numberinput=int(input(">"))
+
+
+if numberinput == number:
+    print("სწორია")
+    
+
+
+
+
+
+
+
