f18c77afcd1021c5376627a293b2bb2d6d87c05c
dachipapashvili <dachipapashvili4@gmail.com>
2025-03-29 16:34:37 +0400

    commit

---
+++ Day 24/Classwork/Classwork.py
@@ -0,0 +1,16 @@
+for i in range(20):
+    if i == 15:
+     continue 
+    print(i)
+
+
+#2 saklaso
+
+num = 0
+while True:
+    num += 1
+    print(num)
+    if num == 14:
+       break
+
+
---
+++ Day 24/Homework/Homework.py
@@ -0,0 +1,42 @@
+#for i in range (101):
+ #   if i in [ 15,27,67,32,99,93]:
+  #      continue
+   # print(i)
+
+
+
+#3 საშინაო
+"""
+num=0
+while True:
+    num+=1
+    if num==23:
+        break
+"""
+#4 საშინაო
+list=[1,2,3,4,5]
+for i in range(2):
+    list.append(i)
+
+
+#5 საშინაო
+list=[[1,2,3],[4,5,6,]]
+print(list[1][2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
