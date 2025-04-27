d35ac90855184dec7c9fb25be90201aff7cd4b3a
dachipapashvili <dachipapashvili4@gmail.com>
2025-04-27 16:20:00 +0400

    commit

---
+++ Day 28/Classwork/Classwork.py
@@ -0,0 +1,47 @@
+# slicingi გამოიყენება listis ელემენტების
+
+"""
+name=input("enter your name")
+name1=input("enter your name")
+
+if name[-6:]=="shvili":
+    print("hello")    
+
+if name1[-3:]=="dze":
+    print("bye")
+"""
+
+
+"""
+winadadeba=input("eneter windadaeba: ")
+indeqsi=int( input("enter you indeqsi: "))
+
+print(winadadeba[0:indeqsi])
+"""
+
+
+age=13
+
+while age<39:
+    print (str(age)+"year")
+    age+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+1)კომენტარის სახით ახსენით რაში გამოიყენება sliceing და რას გადავცემთ მას
+
+4)ცვლადში შეინახეთ თქვენი ასაკი და while ციკლი ამუშავეთ იქამდე სანამ არ მიაღწევს თქვენი (ასაკი * 3)-ს (ყოველ ჯერზე დაბეჭდეთ თქვენი ასაკი და გვერძე მიუწერეთ "year"
+
+5)სიაში შეინახეთ 10 ელემენტი და sliceing-ის მეშვეობით გამოიტანეთ ყველა ლუწ ინდექსზე მდგომი ელემენტი
+"""
