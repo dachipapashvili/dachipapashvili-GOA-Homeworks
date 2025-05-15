ced4c3dc607568e1c5e4d0e64da24b19c93d406c
dachipapashvili <dachipapashvili4@gmail.com>
2025-05-15 22:29:10 +0400

    commit

---
+++ Day 30/Classwork/Classwork.py
@@ -0,0 +1,11 @@
+numbers=[1,2,3,4,5,6,8,9,12,34]
+kenti=0
+luwi=0
+for i in numbers:
+    if i%2 ==0:
+        luwi+=1
+    else:
+        kenti+=1
+print(kenti,luwi)
+
+    
---
+++ Day 30/Homework/Homework.py
@@ -0,0 +1,23 @@
+"""
+print("luka " ,132, 12.12, [12123])
+
+user_input=input("text:")
+
+print("hello")
+
+input("input text:")
+
+print(int("121"))
+print(int("hello"))
+
+print(str([0,1,2,3]))
+
+num=(1,2,3,4,5,6,7,8,9,10)
+print[0:5]
+"""
+
+list=[20,21,22,23,24,25,26,27,28,29,30]
+print(list[::2])
+
+
+
